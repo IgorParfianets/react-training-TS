c23b06824ad9902fdab6b2d8f7d2e62021552055
Igor <igor.parfianets@gmail.com>
2023-02-05 17:43:29 +0300

    Implement useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
 import { Product } from "./components/Product";
-import { products } from "./data/products";
+import { IProduct } from "./models";
 
 function App() {
+  // Hook for state
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  async function fetchProducts() {
+    try {
+      setError("");
+      setLoading(true);
+      const response = await axios.get<IProduct[]>(
+        "https://fakestoreapi.com/products?limit=5"
+      );
+      setProducts(response.data);
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as AxiosError;
+      setLoading(false);
+      setError(error.message);
+    }
+  }
+
+  // Hook for API requests
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product product={products[0]} />
+      {loading && <p className="text-center">Loading...</p>}
+      {error && <p className="text-center text-red">{error}</p>}
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
+
+      {/* <Product product={products[0]} /> */}
     </div>
   );
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,17 +1,34 @@
 import { IProduct } from "../models";
+import { useState } from "react";
 
 interface ProductProps {
   product: IProduct;
 }
 
 export function Product({ product }: ProductProps) {
+  const [details, setDetails] = useState(false);
+
+  const btnBgClassName = details ? "bg-yellow-400" : "bg-blue-700";
+  const btnClasses = ["py-2 px-4 border rounded", btnBgClassName];
+
   return (
     <div className="border p-2 rounded flex flex-col items-center">
       <img src={product.image} className="w-1/3" alt={product.title} />
       <p>{product.title}</p>
-      <br />
-      <p className="text-center">{product.description}</p>
+
       <p className="font-bold">{product.price}$</p>
+      <button
+        className={btnClasses.join(" ")}
+        onClick={() => setDetails((prev) => !prev)}
+      >
+        {details ? "Hide" : "Show"}
+      </button>
+
+      {details && (
+        <div>
+          <p className="text-center">{product.description}</p>
+        </div>
+      )}
     </div>
   );
 }
